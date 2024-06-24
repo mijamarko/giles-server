f93a088e87866481d5c9fcb0e7d2c8f2ab2a6ad8
Marko Mijajlovic <marko.mijajlovic@proton.me>
2024-06-24 21:43:07 +0200

    initial commit

---
+++ src/main/java/com/github/mijamarko/gilesserver/GilesServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.mijamarko.gilesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilesServerApplication.class, args);
+    }
+
+}
